96c5c75979aeb5f0fddd9e6c68ba900aab6592b8
Jared Mackey <jared@mackeydevelopments.com>
2015-11-14 21:50:27 -0700

    Fixed setup.py error

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django-postgreconnect",
-    packages="django-postgreconnect",
+    packages=["django-postgreconnect",],
     version="0.1",
     description="A Django backend that automatically reconnects to postgres if the connection is lost.",
     author="Jared Mackey",
