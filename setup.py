378e9342ec0cadc23c2cab6a9a883f4ee7622474
ecco <none@none.com>
2019-12-03 07:40:17 -0500

    bump version, django 3.0 support

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="django-postgreconnect",
     packages=["django-postgreconnect",],
-    version="0.2",
+    version="0.3",
     description="A Django backend that automatically reconnects to postgres if the connection is lost.",
     author="Jared Mackey",
     author_email="jared@mackeydevelopments.com",
