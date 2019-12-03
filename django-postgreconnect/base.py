b80309575a68a21f68fa642c53bc199e719313a0
ecco <none@none.com>
2019-12-03 07:34:13 -0500

    use postgres instead of postgres_psycopg2 which has been an alias since django 1.9 and is now removed in django 3.0, which breaks this package

--- django-postgreconnect/base.py
+++ django-postgreconnect/base.py
@@ -1,4 +1,4 @@
-from django.db.backends.postgresql_psycopg2.base import DatabaseWrapper as PostgresWrapper
+from django.db.backends.postgresql.base import DatabaseWrapper as PostgresWrapper
 
 
 class DatabaseWrapper(PostgresWrapper):
